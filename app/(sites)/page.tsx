fd01769c6e9d7e27138bb98ffcfcff80c8841dac
chavarahul <chavarahul7@gmail.com>
2024-12-15 21:04:39 +0530

    intiliased project

---
+++ app/(sites)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className="border border-red-500 md:flex">
+        <div className="md:w-2/3"></div>
+        <div className=""></div>
+    </section>
+  );
+}
